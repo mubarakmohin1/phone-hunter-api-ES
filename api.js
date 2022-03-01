88040797251215c37931bd22d66a396fe3857658
mubarakmohin1 <chemistmubarak1@gmail.com>
2022-03-02 00:06:02 +0600

    first commit

--- api.js
+++ api.js
@@ -56,30 +56,50 @@ mainDiv.appendChild(div);
 
 }
 const displayDetails = (uniqe)=>{
-    console.log(uniqe)
+    
 
-
-    const url = ` https://openapi.programming-hero.com/api/phones?search=phone` ;
+    const url = ` https://openapi.programming-hero.com/api/phone/${uniqe}`;
     fetch(url)
     .then(res => res.json())
-    .then(data => {
-        const allPhone = data.data;
-        const singlePhone = allPhone.find(phone => phone.slug === uniqe );
-         const div = document.createElement('div');
-         mainDiv.innerHTML = "";
-         div.innerHTML = `
-         <div class="card" style="width: 18rem;">
-         <img class="w-50" src="${singlePhone.image}"   class="card-img-top" alt="...">
-          <div class="card-body">
-          <h4 class="card-title">${singlePhone.phone_name}</h2>
-          <h6 class="card-title">${singlePhone.brand}</h5>
-          </div>
-          </div>
-         
-         `
-         mainDiv.appendChild(div);
-     
-    })
+    .then(data => moreDetails(data.data))
 }
+    const moreDetails = (uniqePhone) =>{
+        console.log(uniqePhone);
+        
+        
+        // const div = document.createElement9('div');
+        // div.innerHTML = `
+        // <h6>${sinlePhone.mainFeatures.chipSet}</h6>
+        
+        // `
+        // mainDiv.appendChild(div);
+       }
+    
+
+
+    // const url = `  https://openapi.programming-hero.com/api/phone/=${uniqe}` ;
+    // console.log(url);
+    // fetch(url)
+    // .then(res => res.json())
+    // .then(data => {
+    //     const allPhone = data.data;
+    //     const singlePhone = allPhone.find(phone => phone.slug === uniqe );
+    //      const div = document.createElement('div');
+    //      mainDiv.innerHTML = "";
+          
+    //      div.innerHTML = `
+    //      <div class="card" style="width: 18rem;">
+    //      <img class="w-50" src="${singlePhone.image}"   class="card-img-top" alt="...">
+    //       <div class="card-body">
+    //       <h4 class="card-title">${singlePhone.phone_name}</h2>
+    //       <h6 class="card-title">${singlePhone.brand}</h5>
+    //       </div>
+    //       </div>
+         
+    //      `
+    //      mainDiv.appendChild(div);
+     
+    // })
+
 
  
