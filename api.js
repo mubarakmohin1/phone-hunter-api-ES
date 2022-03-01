216d031734aa5868a24206cefc12f53729c2e947
mubarakmohin1 <chemistmubarak1@gmail.com>
2022-03-01 18:40:53 +0600

    first commit

--- api.js
+++ api.js
@@ -1,14 +1,31 @@
 const loadSearchValue = () => {
- const searchValue = document.getElementById('search-value').value;
-  const url = " https://openapi.programming-hero.com/api/phones?";
-  fetch(url)
-  .then(res => res.json())
-  .then(data => displayPhone(data.phones));
-}
-const displayPhone = (phones)=> {
-console.log(phones)
+ const searchInput = document.getElementById('search-value');
+ const searchValue = searchInput.value;
+ const errorHnadle = document.getElementById('error');
+  
+ if(searchValue == ""){
+    errorHnadle.innerText = 'please give me a correct value!!'
+    searchInput.value =""
+
+ }
+  
+else if((searchValue >= 0) || (searchValue < 0)){
+    errorHnadle.innerText = 'please give me a correct value!!'
+    searchInput.value =""
+ }
+ else{
+    const url = ` https://openapi.programming-hero.com/api/phones?search=${searchValue}` ;
+    fetch(url)
+    .then(res => res.json())
+    .then(data => displayPhones(data.data))
+    
+    searchInput.value =""
+ }
+   
 
 }
+const displayPhones = (phones) =>{
+    console.log(phones)
 
- 
+}
  
